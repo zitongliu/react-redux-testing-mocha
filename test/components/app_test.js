f92775030a09f41f283de15e9f118f231516816e
zitongliu <zitongliu7@gmail.com>
2017-08-22 11:23:36 +1000

    first spec

---
+++ test/components/app_test.js
@@ -0,0 +1,17 @@
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app';
+
+// Use 'describe' to group together similar tests
+describe('App', () => {
+
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertio' about a target
+    expect(component).to.contain('React simple starter');
+  });
+
+});
