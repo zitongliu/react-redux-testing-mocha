5649158f1749069c2a344a546f7920ee351374ad
zitongliu <zitongliu7@gmail.com>
2017-08-23 09:52:01 +1000

    created CommentList component and specs

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,6 @@
 import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
-import CommentBox from '../../src/components/app';
+import {CommentBox, CommentList} from '../../src/components/app';
 
 // Use 'describe' to group together similar tests
 describe('App', () => {
@@ -24,5 +24,9 @@ describe('App', () => {
   it('shows a comment box', () => {
     expect(component.find('.comment-box')).to.exist;
   });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
+  })
 
 });
---
+++ test/components/comment_list_test.js
@@ -0,0 +1,21 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+
+
+describe('CommentList', () => {
+
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentList);
+  });
+
+  it('has the correct class', () => {
+    expect(component).to.have.class('comment-list');
+  });
+
+  // it('should show a list of comments', () => {
+  //   expect(component.find('li')).to.exist;
+  // });
+});
