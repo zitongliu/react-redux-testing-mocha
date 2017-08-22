0bedf280c7330c481256d2c6670249db0cb79d75
zitongliu <zitongliu7@gmail.com>
2017-08-22 14:24:39 +1000

    comment box tests wip

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,12 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+  it('has a text area', () => {
+    expect(component)
+  });
+
+  it('it has a button', () => {
+
+  });
+});
